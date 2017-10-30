d1e3084469e1a686a6205338b7ed6ce0dcd4d53f
Rudresha, Gulaganjihalli Parameshappa <rudreshgp18@gmail.com>
2017-10-30 20:17:41 +0100

    Implemented Object functions related to object constructor

--- Exercise 1/objects/flight-es2015.js
+++ Exercise 1/objects/flight-es2015.js
@@ -1,4 +1,30 @@
 // TODO: Re-implement flight.js using ES2015
+class MarsFlight {
+    constructor(outbounddate, speed) {
+        this.speed = speed || 1;
+        this.outbounddate = outbounddate || 'February 19, 2017';
+        this.originplace = 'Earth';
+        this.destinationplace = 'Mars';
+    }
+    duration() {
+        return (63070000000 / this.speed);
+    }
+    durationInDays() {
+        return Math.round(this.duration() / (1000 * 60 * 60 * 24));
+    }
+    getInboundDate() {
+        return new Date(Date.parse(this.outbounddate) + this.duration());
+    }
+}
+
+class FastMarsFlight extends MarsFlight {
+    constructor(outbounddate) {
+        super(outbounddate, 3);
+    }
+}
+var plannedDeparture = 'February 20, 2017';
+var marsFlight = new MarsFlight(plannedDeparture);
+var fastMarsFlight = new FastMarsFlight(plannedDeparture);
 
 console.log(`The RyanAir Mars flight will take ca. ${marsFlight.durationInDays()} days (ETA: ${marsFlight.getInboundDate()}).`);
 
