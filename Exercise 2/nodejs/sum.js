ae4c0412849dc0131eb349614448dd8e5b740f97
Srivatsa S <Srivatsashankaran@gmail.com>
2017-11-08 04:38:54 +0530

    Update sum.js
    Check this

--- Exercise 2/nodejs/sum.js
+++ Exercise 2/nodejs/sum.js
@@ -5,3 +5,14 @@
 // For example:
 // node sum.js 1 2 3
 // 6
+
+//process.argv is the array containing the command line args
+//loop is started from 2 because process.argv has the first 2 elemnts of array as ['path/to/node.exe', 'path/to/js/file', ...]
+
+var sum = 0;
+for(var i =2; i<process.argv.length; i++){
+  //converting to number because process.argv is a string
+  sum = sum + Number(process.argv[i]);
+}
+
+console.log(sum);
